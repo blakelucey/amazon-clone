5dada069d067d49e704bdece05a31d4f380a1362
Blake Lucey <blucey7@gmail.com>
2022-03-25 17:13:17 -0500

    Initial Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button, Input, Space, Badge } from 'antd';
 import { useMoralis } from "react-moralis";
+import { Link } from 'react-router-dom';
 import './Header.css'
+import Amazon from '../images/logo.png';
+import USA from '../images/usa.png';
+import BookStore from '../images/bookstore.png';
+import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+
+
+const { Search } = Input;
+const categories = ["Comics", "Dictionaries", "Novels", "Fantasy", "Horror", "Adventure"];
 
 const Header = () => {
   const { authenticate } = useMoralis();
@@ -10,11 +19,50 @@ const Header = () => {
         ghost={false}
         title="Amazon Clone"
         extra={[
-         <Button key="1" type="primary" onClick={() => authenticate()}>
-          Login with Metamask
+          <>
+          <img src={Amazon} className="logo"></img>
+          <img src={BookStore} className="logo"></img>
+          <Search 
+            placeholder="find a product"
+            enterButton
+            className="searchBar"
+          />
+         <Button key="1" type="primary" onClick={() => authenticate()} className="login">
+          Login
           </Button>
+
+          <Space size={"large"}>
+              
+              <Badge count={0} showZero>
+                <span className="header-buttons">
+                  <ShoppingCartOutlined className="header-icon" />
+                  Cart
+                </span>
+              </Badge>
+              <Space className="header-buttons" size={"small"}>
+                <img src={USA} alt="region" className="flag"></img>▾
+              </Space>
+              
+            </Space>
+          </>
         ]}>
       </PageHeader>
+      <div className="site-page-subheader-ghost-wrapper">
+      <Space size={"middle"}>
+        <Space size={"small"} style={{fontWeight:"bold"}}>
+          <MenuOutlined />
+          Categories
+        </Space>
+        {categories.map((e) =>{
+          return(
+            <Link to="/categories" state={e} className="categories">
+              {e}
+            </Link>
+          )
+
+        })}
+      </Space>
+    </div>
     </div>
   )
 }
